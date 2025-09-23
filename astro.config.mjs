cf694642df27cd49002d9324efa567389ab8c47f
Dave Mateer <davemateer@gmail.com>
2025-09-23 13:18:24 +0100

    added in node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import node from '@astrojs/node';
+
+export default defineConfig({
+  output: 'server',          // <-- needed for SSR
+  adapter: node({
+    mode: 'standalone'       // bundles deps so Render can run it easily
+  }),
+});
