8c8b62d7d4fa09a50664de1c55b8602b69177938
Dave Mateer <davemateer@gmail.com>
2025-09-23 20:21:24 +0100

    2 minute session cookie timout for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'       // bundles deps so Render can run it easily
   }),
+  session: {
+    ttl: 120               // 1 day timeout (24 hours * 60 minutes * 60 seconds)... test here with 120 seconds
+  },
   server: {
     host: '0.0.0.0',         // Bind to all interfaces for Render
     port: process.env.PORT ? parseInt(process.env.PORT) : 10000
